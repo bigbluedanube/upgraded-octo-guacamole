5cdc9b49c782da7eacdfbfdf70b373e42989b35f
Nicole Lang <nicole.lang@revature.net>
2020-10-20 11:13:56 -0400

    hard-coded moods and messages
    -randomly prints 3 sets of mood/message

--- moodsMiniProject/src/main/java/com/revature/MoodsMiniProjectApplication.java
+++ moodsMiniProject/src/main/java/com/revature/MoodsMiniProjectApplication.java
@@ -1,13 +1,53 @@
 package com.revature;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.revature.models.Message;
+import com.revature.models.Mood;
 
 @SpringBootApplication
 public class MoodsMiniProjectApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MoodsMiniProjectApplication.class, args);
+		//SpringApplication.run(MoodsMiniProjectApplication.class, args);
+		
+		//hard-coded moods and messages
+		String[] moodStrings = {"Tired", "Happy", "Sad", "Angry", "Disgusted", "Fearful", "Excited", 
+				"Uncertain", "Bored", "Adoring", "Pouting", "Mischievous", "Sarcastic", "Rushed/Wired", "Musical"};
+		String[] messageStrings = {"My hovercraft is full of eels.", "I turned around for five minutes!", 
+				"Put that thing back where it came from or so help me.", "Who’s a good boy?", "My dog ate my homework.",
+				"It seemed like a good idea at the time.", "Where have you been?", "Do you wanna build a snowman?",
+				"I tawt I taw a puddy tat!", "It’s turtles all the way down.", "Coffee is the lifeblood that drives the dreams of champions.",
+				"Did you say thirty million or three trillion?", "Begin Operation: Upgraded Octo-Guacamole.", "Happy Rama-hana-kwanz-mas-tice.",
+				"I don’t have enough caffeine for this."};
+		
+		List<Mood> moods = new ArrayList<Mood>();
+		List<Message> messages = new ArrayList<Message>();
+		Mood mo;
+		Message me;
+		
+		for(int i = 1; i <= moodStrings.length; i++) {
+			mo = new Mood(i, moodStrings[i-1]);
+			moods.add(mo);
+		}
+		for(int i = 1; i <= messageStrings.length; i++) {
+			me = new Message(i, messageStrings[i-1]);
+			messages.add(me);
+		}
+		
+		for(int i = 0; i < 3; i++)
+		{
+			mo = moods.get(new Random().nextInt(moods.size()));
+			me = messages.get(new Random().nextInt(messages.size()));
+			System.out.println("Mood: " + mo.getName());
+			System.out.println("Message: " + me.getText());
+			System.out.println();
+		}
 	}
 
 }
---
+++ moodsMiniProject/src/main/java/com/revature/models/Mood.java
@@ -0,0 +1,62 @@
+package com.revature.models;
+
+public class Mood {
+
+	private int id;
+	private String name;
+	
+	public Mood() {
+		super();
+	}
+	public Mood(String name) {
+		super();
+		this.name = name;
+	}
+	public Mood(int id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	@Override
+	public String toString() {
+		return "Mood [id=" + id + ", name=" + name + "]";
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Mood other = (Mood) obj;
+		if (id != other.id)
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
+}
